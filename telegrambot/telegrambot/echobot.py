2fd72627962e4d01715f641fac626de0776f1536
Tan Gian Sen <64723554+GSgiansen@users.noreply.github.com>
2025-08-13 12:41:44 +0800

    fix/Add miniapp as regular folder

--- telegrambot/telegrambot/echobot.py
+++ telegrambot/telegrambot/echobot.py
@@ -28,7 +28,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token().build()
+    application = Application.builder().token("7952714514:AAFU4Lfxwv4IC47xEhwG2ByKMwZLfyf9aMw").build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
