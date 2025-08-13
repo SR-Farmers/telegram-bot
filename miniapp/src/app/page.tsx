929bd29fcc8e2cdac3e49b853ee71d8635c43843
Tan Gian Sen <64723554+GSgiansen@users.noreply.github.com>
2025-08-13 13:00:31 +0800

    feat/add new paths and pages

--- miniapp/src/app/page.tsx
+++ miniapp/src/app/page.tsx
@@ -7,8 +7,6 @@ import { Link } from '@/components/Link/Link';
 import { LocaleSwitcher } from '@/components/LocaleSwitcher/LocaleSwitcher';
 import { Page } from '@/components/Page';
 
-import tonSvg from './_assets/ton.svg';
-
 export default function Home() {
   const t = useTranslations('i18n');
 
@@ -16,41 +14,22 @@ export default function Home() {
     <Page back={false}>
       <List>
         <Section
-          header="Features"
-          footer="You can use these pages to learn more about features, provided by Telegram Mini Apps and other useful projects"
+          header="RC4ttendance Bot"
+          footer="Log your attendance or make your own event"
         >
-          <Link href="/ton-connect">
-            <Cell
-              before={
-                <Image
-                  src={tonSvg.src}
-                  style={{ backgroundColor: '#007AFF' }}
-                  alt="TON Logo"
-                />
-              }
-              subtitle="Connect your TON wallet"
-            >
-              TON Connect
-            </Cell>
-          </Link>
         </Section>
         <Section
-          header="Application Launch Data"
-          footer="These pages help developer to learn more about current launch information"
-        >
-          <Link href="/init-data">
-            <Cell subtitle="User data, chat information, technical data">
-              Init Data
+          header="Create your own event"
+          footer="Done by Gian Sen, Raihan, Cavan, Guanlin, Jin Yuan"
+        >         
+        <Link href="/create-poll">
+            <Cell subtitle="Create your poll for your IG!">
+              Create Poll
             </Cell>
           </Link>
-          <Link href="/launch-params">
-            <Cell subtitle="Platform identifier, Mini Apps version, etc.">
-              Launch Parameters
-            </Cell>
-          </Link>
-          <Link href="/theme-params">
-            <Cell subtitle="Telegram application palette information">
-              Theme Parameters
+          <Link href="/user-profile">
+            <Cell subtitle="Check which groups you have admin for">
+              User Profile
             </Cell>
           </Link>
         </Section>
